63ebb9a2920ad96300d9b95fab69823a2b7538fa
Alexander Golubev <a.golubevspb1608@gmail.com>
2026-08-09 20:54:15 +0300

    - Исправил логику работы методов, по добавлению, удалению друзей

--- src/main/java/ru/yandex/practicum/filmorate/storage/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/service/UserService.java
@@ -4,11 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.filmorate.exceptions.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exceptions.FriendshipAlreadyExistsException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundDataException;
@@ -16,8 +11,7 @@ import ru.yandex.practicum.filmorate.model.Friendship;
 import ru.yandex.practicum.filmorate.model.FriendshipStatus;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.validator.Create;
-import ru.yandex.practicum.filmorate.validator.Update;
+
 
 import java.util.Collection;
 import java.util.Map;
@@ -29,21 +23,6 @@ public class UserService {
     private final Logger log = LoggerFactory.getLogger(UserService.class);
     private final InMemoryUserStorage inMemoryUserStorage;
 
-    @GetMapping
-    public Collection<User> getUsers() {
-        return inMemoryUserStorage.getUsers();
-    }
-
-    @PostMapping
-    public User createUser(@Validated(Create.class) @RequestBody User user) {
-        return inMemoryUserStorage.createUser(user);
-    }
-
-    @PutMapping
-    public User updateUser(@Validated(Update.class) @RequestBody User user) {
-        return inMemoryUserStorage.updateUser(user);
-    }
-
     public Map<String, String> addFriend(Long id, Long friendId) {
         checkUserOrThrow(id, friendId);
 
@@ -51,6 +30,10 @@ public class UserService {
         if (user.getFriends().containsKey(friendId)) {
             log.warn("Попытка добавить в друзья пользователя, который уже находится в списке друзей");
             throw new FriendshipAlreadyExistsException(id, friendId);
+        } else if (user.getRequestsFriendsSetList().getIncomingUsersSetList().contains(friendId)) {
+            log.warn("Попытка добавить в друзья пользователя, который отправил пользователю заявку на добавление");
+            throw new DuplicatedDataException(String.format("Пользователь %s уже отправил вам заявку на добавления в " +
+                    "друзья. Подтвердите добавление или удалить его из друзей.", friendId));
         }
         user.getFriends().put(friendId, new Friendship(id, friendId, FriendshipStatus.PENDING));
         user = inMemoryUserStorage.getUsersMap().get(friendId);
@@ -77,20 +60,27 @@ public class UserService {
         User friend = inMemoryUserStorage.getUsersMap().get(friendId);
         Friendship friendship = friend.getFriends().get(id);
         friendship.setStatus(FriendshipStatus.ACCEPTED);
-        friend.getFriends().put(id, friendship);
         user.getFriends().put(friendId, friendship);
         log.info("Дружба между {} и {} успешно подтверждена.", id, friendId);
         user.getRequestsFriendsSetList().getIncomingUsersSetList().remove(friendId);
         log.info("Пользователь {} удален из списка входящих заявок на добавление", friendId);
         return Map.of("message", "Дружба подтверждена!😎");
-
     }
 
     public Map<String, String> deleteFriend(Long id, Long friendId) {
         checkUserOrThrow(id, friendId);
         User user = inMemoryUserStorage.getUsersMap().get(id);
+        if (user.getRequestsFriendsSetList().getIncomingUsersSetList().contains(friendId)){
+            user.getRequestsFriendsSetList().getIncomingUsersSetList().remove(friendId);
+            return Map.of("message", "Пользователь успешно удален");
+        }
         user.getFriends().remove(friendId);
+
         user = inMemoryUserStorage.getUsersMap().get(friendId);
+        if (user.getRequestsFriendsSetList().getIncomingUsersSetList().contains(id)){
+            user.getRequestsFriendsSetList().getIncomingUsersSetList().remove(id);
+            return Map.of("message", "Пользователь успешно удален");
+        }
         user.getFriends().remove(id);
         return Map.of("message", "Пользователь успешно удален");
     }
@@ -98,8 +88,9 @@ public class UserService {
     public Collection<User> getFriendsUser(Long id) {
         checkUserOrThrow(id);
         User user = inMemoryUserStorage.getUsersMap().get(id);
-        return user.getFriends().keySet().stream()
-                .map(friendId -> inMemoryUserStorage.getUsersMap().get(friendId))
+        return user.getFriends().entrySet().stream()
+                .filter(entry -> entry.getValue().getStatus() == FriendshipStatus.ACCEPTED)
+                .map(entry -> inMemoryUserStorage.getUsersMap().get(entry.getKey()))
                 .toList();
     }
 
@@ -109,9 +100,10 @@ public class UserService {
         User user = inMemoryUserStorage.getUsersMap().get(id);
         User otherUser = inMemoryUserStorage.getUsersMap().get(otherId);
 
-        return user.getFriends().keySet().stream()
-                .filter(friend -> otherUser.getFriends().containsKey(friend))
-                .map(friend -> inMemoryUserStorage.getUsersMap().get(friend))
+        return user.getFriends().entrySet().stream()
+                .filter(entry -> otherUser.getFriends().containsKey(entry.getKey()) && otherUser.getFriends().get(entry.getKey()).getStatus() == FriendshipStatus.ACCEPTED &&
+                        user.getFriends().get(entry.getKey()).getStatus() == FriendshipStatus.ACCEPTED)
+                .map(entry -> inMemoryUserStorage.getUsersMap().get(entry.getKey()))
                 .toList();
     }
 
